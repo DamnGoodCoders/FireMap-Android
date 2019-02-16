993ad4d8e4234d8cc05de3cb61529365f8eee1fc
Joshua F. Mathews <JoshFeltonM@gmail.com>
2019-02-15 17:50:41 -0700

    Added gdal dependency.

--- app/src/main/java/com/example/firemap/MainActivity.kt
+++ app/src/main/java/com/example/firemap/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.firemap
-
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +10,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import org.w3c.dom.Text
+import org.gdal.gdal.gdal
 
 class MainActivity : AppCompatActivity() {
 
