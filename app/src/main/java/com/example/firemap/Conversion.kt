0ff92b02683b3559bfd4f15009688da7b377ddc5
Caleb Gartner <calebgtnr@gmail.com>
2019-02-28 16:26:30 -0700

    Application now opens default file chooser on button click

--- app/src/main/java/com/example/firemap/Conversion.kt
+++ app/src/main/java/com/example/firemap/Conversion.kt
@@ -1,10 +1,13 @@
 package com.example.firemap
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_conversion.*
+
+private const val READ_REQUEST_CODE: Int = 42
 
 class Conversion : AppCompatActivity() {
 
@@ -13,9 +16,45 @@ class Conversion : AppCompatActivity() {
         setContentView(R.layout.activity_conversion)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            performFileSearch()
+        }
+    }
+
+    private fun performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            // Filter to only show results that can be "opened", such as a
+            // file (as opposed to a list of contacts or timezones)
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+            // Filter to show only images, using the image MIME data type.
+            // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+            // To search for all documents available via installed storage providers,
+            // it would be "*/*".
+            type = "*/pdf"
+        }
+
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            resultData?.data?.also { uri ->
+//                Log.i(TAG, "Uri: $uri")
+//                showImage(uri)
+            }
         }
     }
 
