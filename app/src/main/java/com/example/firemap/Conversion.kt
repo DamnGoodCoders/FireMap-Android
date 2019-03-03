0e7a58cd9dd126cb331c9fe6fbc02fb5e0d81299
Caleb Gartner <calebgtnr@gmail.com>
2019-03-02 22:42:23 -0700

    Changed 'minSdkVersion' from 19 -> 21 to support the 'ACTION_OPEN_DOCUMENT_TREE' intent necessary for selecting an output directory Additional alertdialog string resource Added two more floating action buttons and changed their ids to match their functionality Added flexible Alert Dialog fragment that can display different messages/titles Added basic functionality for all three necessary actions - choosing an output directory, choosing the pdf(s) to convert, and the actual conversion button itself

--- app/src/main/java/com/example/firemap/Conversion.kt
+++ app/src/main/java/com/example/firemap/Conversion.kt
@@ -11,73 +11,77 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_conversion.*
 
 import java.util.*
-import java.io.File
 
 import org.gdal.gdal.gdal
-import android.content.DialogInterface
-import android.app.Dialog
-import android.support.v7.app.AlertDialog
 
-import com.example.firemap.FailureAlertDialog
-
-
-private const val READ_REQUEST_CODE: Int = 42
+private const val MAP_REQUEST_CODE: Int = 42
+private const val DEST_REQUEST_CODE: Int = 73
 
 
 class Conversion : AppCompatActivity() {
 
-    // TODO add TranslateOptions String vector
-    // TODO map the functionality to a nicer/more streamlined layout later; get it working first
-    private val optionsVector: Vector<String> = Vector()
+    // TODO map this functionality to a nicer/more streamlined layout later; get it working first
+
     private val TAG = "Conversion"  // logging tag
-
-    // TODO add button to choose output directory -> save uri/path
-
+    private val defaultDir = "Downloads"  // for use if the destination dir is ever null, which should never happen, but hey
+    private var destinationDir: String? = defaultDir  // TODO get default location from Brad
     private val fileURIQueue: ArrayDeque<Uri> = ArrayDeque()  // unknown number of elements
+    private val optionsVector: Vector<String> = Vector(4)
+
+    init {
+        optionsVector.add("-of")  // output format
+        optionsVector.add("GTiff")
+        optionsVector.add("")  // Indexes 3 and 4 are the input and output files, respectively
+        optionsVector.add("")
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversion)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
+        add.setOnClickListener {
             performFileSearch()
+        }
+        cvt.setOnClickListener {
+            convertMapPDFToTiff()
+        }
+        dst.setOnClickListener {
+            getDestinationDir()
         }
     }
 
-    private fun performFileSearch() {
+    private fun getDestinationDir() {
+        // TODO display destination dir in UI somewhere
+        // ACTION_OPEN_DOCUMENT_TREE requires minSdkVersion 21+
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        startActivityForResult(intent, DEST_REQUEST_CODE)
+    }
 
+    private fun performFileSearch() {
         // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file browser.
-        // use 'ACTION_GET_CONTENT' instead?
         // 'apply' is essentially a factory builder
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-            // Filter to only show results that can be "opened", such as a
-            // file (as opposed to a list of contacts or timezones)
+            // Filter to only show pdfs that can be "opened"
             addCategory(Intent.CATEGORY_OPENABLE)
             putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-//            DocumentsContract.EXTRA_INITIAL_URI will open the chooser at this initial location
-
-            // Filter to show only pdfs, using the application/pdf data type.
-            // To search for all documents available via installed storage providers,
-            // it would be "*/*".
-            type = "*/*"  // TODO limit to pdfs . . . 'x-pdf'?
+            type = "application/pdf"
         }
 
-        startActivityForResult(intent, READ_REQUEST_CODE)
+        startActivityForResult(intent, MAP_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
 
         // The ACTION_OPEN_DOCUMENT intent was sent with the request code
-        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // *REQUEST_CODE. If the request code seen here doesn't match, it's the
         // response to some other intent, and the code below shouldn't run at all.
 
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // The document selected by the user won't be returned in the intent.
-            // Instead, a URI to that document will be contained in the return intent
+        if (requestCode == MAP_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document(s) selected by the user won't be returned in the intent.
+            // Instead, a URI to each document will be contained in the return intent
             // provided to this method as a parameter.
-            // Pull that URI using resultData.getData()/getClipData().
-            // TODO ensure that output directory has been specified first - get default location from Brad
             resultData?.clipData?.also { clip ->
                 for (idx in 0..(clip.itemCount - 1)) {
                     Log.i(TAG, "Clip URI: ${clip.getItemAt(idx).uri}")
@@ -89,44 +93,56 @@ class Conversion : AppCompatActivity() {
                 fileURIQueue.add(uri)
             }
 
-            // TODO move the below to a separate button/method
-            if (isExternalStorageWritable()) {
-//                Log.i(TAG, "DownloadDir: ${Environment.DIRECTORY_DOWNLOADS}")
-//                optionsVector.clear()  // TODO move to constructor/setup; create TranslateOptions
-//                optionsVector.add("-of")
-//                optionsVector.add("GTiff")
-
-                for (uri in fileURIQueue) {
-                    Log.i(TAG, "Write URI: $uri")
-                    Log.i(TAG, "Write path: ${uri.path}")
-                    convertMapPDFToTiff(uri)
+        } else if (requestCode == DEST_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.also { uri ->
+                Log.i(TAG, "DESTINTAION URI: $uri")
+                if (uri.path != null) {
+                    destinationDir = uri.path
                 }
-                fileURIQueue.clear()  // TODO show queue as ListView/RecycleView on screen
-            } else {
-                notifyOnFailure()
-                Log.i(TAG, "External storage not available to write; conversion is impossible")
             }
-        } // no need for 'else' statement, the call just went to a different 'intent' action
+        }
+        // No need for 'else' statement, the call just went to a different 'intent' action
     }
 
     // TODO add pdf translation here
     // TODO Ensure that conversion CANNOT fail silently - dangerous - use notifyOnFailure
-    fun convertMapPDFToTiff(uri: Uri) {
-        // TODO register drivers, create dataset, etc.
+    fun convertMapPDFToTiff() {
+        if (isExternalStorageWritable()) {
+            if (fileURIQueue.isEmpty()) {
+                notifyOnFailure(message = "No files to convert; none selected")
+                return
+            }
+        // TODO register drivers, create dataset, opts, etc.
 //        gdal.AllRegister()
 //        val ds = gdal.Open(uri.path)
 //        Environment.getExternalStoragePublicDirectory() // gives File
 
-//        gdal.Translate(Environment.DIRECTORY_DOWNLOADS, ds, )
+//        gdal.Translate(Environment.DIRECTORY_DOWNLOADS, ds, opts)
+            for (uri in fileURIQueue) {
+                Log.i(TAG, "Write URI: $uri")
+                Log.i(TAG, "Write path: ${uri.path}")
+            }
+            fileURIQueue.clear()  // TODO show queue as ListView/RecycleView on screen; show progress as well
+        } else {
+            notifyOnFailure(message = "External storage not available to write")
+        }
     }
 
     fun isExternalStorageWritable(): Boolean {
         return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
     }
 
-    fun notifyOnFailure() {
+    fun notifyOnFailure(title: String? = null, message: String? = null) {
         val fragmentManager = supportFragmentManager
         val failureAlert = FailureAlertDialog()
+
+        if (title != null) {
+            failureAlert.setTitle(title)
+        }
+        if (message != null) {
+            failureAlert.setMessage(message)
+        }
+
         failureAlert.show(fragmentManager, "failure_confirmation")
     }
 
--- app/src/main/java/com/example/firemap/FailureAlertDialog.kt
+++ app/src/main/java/com/example/firemap/FailureAlertDialog.kt
@@ -8,18 +8,33 @@ import android.support.v4.app.DialogFragment
 
 class FailureAlertDialog : DialogFragment() {
 
+    private var dialogTitle: CharSequence = "Conversion Failed"  // defaults
+    private var dialogMessage: CharSequence? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
-            builder.setTitle(R.string.dialog_confirm_failure)
+            if (dialogMessage != null) { builder.setMessage(dialogMessage) }
+            builder.setTitle(dialogTitle)
                 .setPositiveButton(R.string.ok,
                     DialogInterface.OnClickListener { dialog, id ->
-                        // Confirmation - do nothing
+                        // Require Confirmation - do nothing
                         dismissAllowingStateLoss()
                     })
             // Create the AlertDialog object and return it
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
+
+    // Fragments should all have empty constructors, so we must provide setters for the title/dialogMessage
+    // String -> CharSequence coercion
+    fun setTitle(title: String) {
+        dialogTitle = title
+    }
+
+    fun setMessage(message: String) {
+        dialogMessage = message
+    }
+
 }
